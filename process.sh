2b197c867e7186a3a6901480b2eaf51b387d1744
Niko Matsakis <niko@alum.mit.edu>
2016-11-18 11:45:28 -0500

    extend process.sh to support patch stages
    The idea is that we want to support incremental tests where we do some
    base build step and then apply a series of patches. We want to track the
    compilation time after each of these patches separately. The model is
    that from one directory $DIR we will create N files, one for each
    patch. They will be named $DIR@$PATCH -- they are basically independent
    tests from the POV of the front-end.
    
    This is achieved by extending the makefile protocol mildly. There is now
    a new target `make patches` that emits a list of patch names. These
    should include the separator (e.g., `@foo`, `@bar`).
    
    If the list of patches returned is empty, the code works as before.
    We execute '(make all; make touch)+ make clean'. The results from
    'make all' are stored in `$dir--$DATE--$i`.
    
    If the last of patches is *non-empty* (say '@A @B'), then we execute
    '(make all@A; make all@B; make touch)+ make clean'. The results from
    'make all' are stored in `$dir@A--$DATE--$i` and `$dir@B--$DATE--$i`.
    
    The naming convention for variants is as follows:
    
        @NNN-text
    
    where NN is 000, 010, 020, 030, etc, and text is some useful description.
    The @ is a distinctive separator we can special-case in the front-end
    later.

--- process.sh
+++ process.sh
@@ -1,37 +1,66 @@
 #!/bin/bash
 
-TIMES_DIR=/home/ncameron/times
-SCRIPTS_DIR=/home/ncameron/times-scripts
+if [ -z "$TIMES_DIR" ]; then
+    TIMES_DIR=/home/ncameron/times
+fi
+if [ -z "$SCRIPTS_DIR" ]; then
+    SCRIPTS_DIR=/home/ncameron/times-scripts
+fi
+
+echo TIMES_DIR=$TIMES_DIR
+echo SCRIPTS_DIR=$SCRIPTS_DIR
 
 START=$(pwd)
 export CARGO_RUSTC_OPTS="-Ztime-passes -Zinput-stats"
 export PATH=$RUSTC_DIR/bin:$PATH
 
-for dir in *; do
+# Check if user provided list of directories;
+# else process them all.
+if [ "$1" != "" ]; then
+    DIRS="$@"
+else
+    DIRS="*"
+fi    
+
+for dir in $DIRS; do
     if [[ -d $dir ]]; then
         echo "Processing $dir"
 
+        cd $START/$dir
+        PATCHES=($(make patches))
+        if [ ! "${PATCHES[*]}" ]; then
+            PATCHES=('')
+        fi
+
+        echo Patches: ${PATCHES[*]}
+
         for i in 0 1 2 3 4 5
         do
-            cd $RUST_DIR
-            git show HEAD -s >$TIMES_DIR/raw/$dir--$DATE--$i.log
-            cd $START/$dir
-            echo "rustc: ./$dir" >>$TIMES_DIR/raw/$dir--$DATE--$i.log
-            make >>$TIMES_DIR/raw/$dir--$DATE--$i.log
-            echo "done" >>$TIMES_DIR/raw/$dir--$DATE--$i.log
-
+            for PATCH in "${PATCHES[@]}"; do
+                cd $RUST_DIR
+                git show HEAD -s >$TIMES_DIR/raw/$dir$PATCH--$DATE--$i.log
+                cd $START/$dir
+                echo "rustc: ./$dir" >>$TIMES_DIR/raw/$dir$PATCH--$DATE--$i.log
+                make all$PATCH >>$TIMES_DIR/raw/$dir$PATCH--$DATE--$i.log
+                echo "done" >>$TIMES_DIR/raw/$dir$PATCH--$DATE--$i.log
+            done
             make touch >/dev/null
         done
 
         make clean >/dev/null
 
         cd $TIMES_DIR
-        python $SCRIPTS_DIR/process.py $dir $DATE 6
+        for PATCH in "${PATCHES[@]}"; do
+            python $SCRIPTS_DIR/process.py "$dir$PATCH" "$DATE" 6
+        done
+
         for i in 0 1 2 3 4 5
         do
-            git add raw/$dir--$DATE--$i.log
-            git add raw/orbit-$dir--$DATE--$i.log
-            git add processed/$dir--$DATE--$i.json
+            for PATCH in "${PATCHES[@]}"; do
+                git add "raw/$dir$PATCH--$DATE--$i.log"
+                git add "raw/orbit-$dir$PATCH--$DATE--$i.log"
+                git add "processed/$dir$PATCH--$DATE--$i.json"
+            done
         done
 
         cd $START
